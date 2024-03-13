df8cb41591b6ef4e72d4d642f889fa60e275c611
Okhtay Wahid Far <120825710+Wambo30@users.noreply.github.com>
2024-03-13 08:04:45 +0100

    Add files via upload
    This repository contains projects that were programmed in C++. It covers topics such as classes, inheritance and data structures.

---
+++ ArrayZeiger/Zeiger.cpp
@@ -0,0 +1,26 @@
+/*
+bearbeitung zeiger aus skript 1b
+
+*/
+
+#include <iostream>
+#include "ArrayZeigerFkt.h"
+
+
+int main()
+{
+
+
+
+
+
+
+
+/******** TESTCODEBEREICH *************************************************************************/
+
+
+
+/**************  ENDE TESTCODEBEREICH  ****************************************************************/
+
+	return 0;
+}
---
+++ blatt24TheoAufg1/blatt24TheoAufg1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	//aufg1.1
+
+	/*
+	float& x =  0;	
+	float y = x;	// falsch, da man zuerst die initialisierung der variable macht und dann erst 
+	// die referenz
+	double& z = y;	// falsch, da y datentyp int hat und z datentyp double hat
+
+	cout << "ausgabe von z: " << z << endl;
+
+	*/
+
+	//aufg.1.2
+
+	/*
+	int& ref;	// referenzbildung geht nur mit syntax : typ& name = VariablenName;
+	int eins = 1;
+	ref = eins;
+
+
+	*/
+
+
+	return 0;
+}
